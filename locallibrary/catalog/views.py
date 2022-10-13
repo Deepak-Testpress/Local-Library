5ad0a73238dbc50bb182d675cbc3fb00d1f02a93
Deepak-Testpress <deepak@testpress.in>
2022-10-13 15:40:13 +0530

    Create Homepage

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    genres = Genre.objects.count()
+
+    code_books = Book.objects.filter(title__contains='code')
+    number_code_books = code_books.count()
+
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    	'genres':genres,
+        'number_code_books':number_code_books,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,6 +19,9 @@ from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from catalog import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

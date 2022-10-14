3aca3827f5c1d30067403bf1ee1404bfa675ace1
Deepak-Testpress <deepak@testpress.in>
2022-10-14 15:01:07 +0530

    Add session

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -19,12 +19,15 @@ def index(request):
     code_books = Book.objects.filter(title__contains='code')
     number_code_books = code_books.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     	'genres':genres,
         'number_code_books':number_code_books,
     }
